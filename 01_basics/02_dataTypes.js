3617bc8f43942a97c70ac3705227da9602e8646c
Saurav Singh <sauravbhadauriya17@gmail.com>
2025-12-10 12:15:48 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using node.js, not browser
+
+console.log(3 + 3)
+console.log("Saurav");
+
+
+
+// datatypes => 
+
+
+let name = "saurav"         // string => ""
+let age = 24                // number => 2 to pwer 53
+let isLoggedIn = false      // boolean => true/false
+let state;                  // undifined => 
+                            // bigint
+                            // null => standalone value
+                            // symbol
+
+
+// Object
+
+console.log(typeof "saurav");
+console.log(typeof age);
+console.log(typeof null);       // object
+console.log(typeof undefined);  // undifined
+
+
