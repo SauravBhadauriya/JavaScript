94ab295b76eaa0024bb4373732fa1734c0972a9d
Saurav Singh <sauravbhadauriya17@gmail.com>
2025-12-10 12:46:15 +0000

    converstion datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "saurav"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+/* check boolean */
+
+
+// let isLoggedIn = 1               //true
+// let isLoggedIn = ""              //false
+let isLoggedIn = "saurav"        //true
+
+
+let booleanIsLogedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLogedIn);
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
