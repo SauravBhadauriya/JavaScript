ca317aaa7fb1eee72d8a7df3c0418e591b52e1da
Saurav Singh <sauravbhadauriya17@gmail.com>
2025-12-10 13:51:27 +0000

    memories

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,4 +1,4 @@
-// Primitive
+// 1st. Primitive
 
 // 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
 
@@ -12,13 +12,13 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 15545485756487987852n
 
 
 
-// Reference (Non Primitive)
+// 2nd. Reference (Non Primitive)
 
 // Array, Objects, Functions
 
@@ -32,12 +32,41 @@ const myFunction = function(){
     console.log("Hello World");
 }
 
-console.log(typeof bigNumber);
-console.log(typeof outsideTemp);
-console.log(typeof scoreValue);
-console.log(typeof myFunction);
-console.log(typeof heros);
+// console.log(typeof bigNumber);
+// console.log(typeof outsideTemp);
+// console.log(typeof scoreValue);
+// console.log(typeof myFunction);
+// console.log(typeof heros);
 
 
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+
+//------------------------Memories-------------------
+
+// Stack (Primitive), Heap (NoN-Primitive)
+
+let myInstaname = "saurav_thakur17"
+
+let anothername = myInstaname
+anothername = "Sauravbhadauriya17"
+
+
+console.log(myInstaname);
+console.log(anothername);
+
+
+let userOne = {
+    email: "user@example.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "user@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
